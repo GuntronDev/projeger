177e438d912694f3282638b4d70c9c66628ffd17
filip-jozif <filip@laptop-05k3khkm-filip.home>
2025-03-13 23:46:55 +0100

    removed 'open dev tools' and resized window - project ready for implementing features

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
@@ -19,9 +19,6 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
