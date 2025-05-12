9ee7a874354b0cb4e0447cd83ecd600599598865
filip-jozif <filip@laptop-05k3khkm-filip.home>
2025-05-12 22:54:41 +0200

    added: arch backbone -> needs implementing

--- src/Arch.svelte
+++ src/Arch.svelte
@@ -1,5 +1,22 @@
 <script>
+    class archModule{
+        packageForJSON() {
+            console.log("not implemented yet");
+        };
+    };
 
+    class workersModule extends archModule{
+
+    };
+    class workerRecord{
+        constructor(role, name, surname, phoneNumber, mail){
+            this.role = role;
+            this.name = name;
+            this.surname = surname;
+            this.phoneNumber = phoneNumber;
+            this.mail = mail;
+        }
+    };
 </script>
 
 <div id="arch-container">
